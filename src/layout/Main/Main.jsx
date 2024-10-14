654abcbf4cba1050872cb24c2745f3f8ddcf2c27
ivi0540 <ivi0540@gmail.com>
2024-10-14 20:08:32 +0300

    feat: add main and footer

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,7 +1,21 @@
-import "./style.css"
+import "./style.css";
+import React from "react";
 
-function Main() {
-  return <main className="main"></main>;
+class Main extends React.Component {
+  state = {
+    movies: [],
+    loaded: false,
+  };
+
+  componentDidMount() {
+    fetch("https://www.omdbapi.com/?apikey=11ef8988&s=matrix&page=1")
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search, loaded: true }));
+  }
+
+  render() {
+    return <main className="main"></main>;
+  }
 }
 
 export { Main };
