e3acfe21a5ffb4cb0270f3fe60f34a4fe79c5668
ivi0540 <ivi0540@gmail.com>
2024-11-07 21:28:14 +0300

    feat: finish project

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import "./style.css"
 
 function Footer() {
   return (
-    <footer className="page-footer footer">
+    <footer className="page-footer footer #1de9b6 teal accent-3">
       <div className="container">
         © {new Date().getFullYear()} Copyright Text
         <a className="grey-text text-lighten-4 right" href="#!">
