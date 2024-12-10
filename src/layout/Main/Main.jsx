eafab8ff4a5470098e12773636f16a7a2204dd89
ivi0540 <ivi0540@gmail.com>
2024-12-10 20:44:50 +0300

    refactor: make the main component a functional component

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,48 +1,45 @@
 import "./style.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Movies } from "../../components/Movies/Movies";
 import { Preloader } from "../../components/Preloader/Preloader";
 import { Search } from "../../components/Search/Search";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-const API_KEY_2 = "11ef8988";
+// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = "11ef8988";
 
-class Main extends React.Component {
-  state = {
-    movies: [],
-    loaded: false,
-  };
+const Main = () => {
+  const [movies, setMovies] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
-  filterMovies = (movieName, movieType) => {
-    this.setState({ loaded: false });
-
+  const filterMovies = (movieName, movieType) => {
+    setLoaded(false);
     fetch(
-      `https://www.omdbapi.com/?apikey=${API_KEY_2}&s=${movieName}${
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${movieName}${
         movieType !== "all" ? `&type=${movieType}` : ""
       }&page=1`
     )
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loaded: true }))
+      .then((data) => {
+        setMovies(data.Search);
+        setLoaded(true);
+      })
       .catch((err) => {
         console.error(`ОШИБКА ${err.massage}`);
       });
   };
 
-  componentDidMount() {
-    this.filterMovies("matrix", "all");
-  }
+  useEffect(() => {
+    filterMovies("matrix", "all");
+  }, []);
 
-  render() {
-    const { movies, loaded } = this.state;
-    return (
-      <>
-        <Search filterMovies={this.filterMovies} />
-        <main className="main">
-          {!loaded ? <Preloader /> : <Movies movies={movies} />}
-        </main>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Search filterMovies={filterMovies} />
+      <main className="main">
+        {!loaded ? <Preloader /> : <Movies movies={movies} />}
+      </main>
+    </>
+  );
+};
 
 export { Main };
